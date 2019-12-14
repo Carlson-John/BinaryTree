9e1e7ca5313c1912b3148e6ade0bb80a8929e93b
John Carlson <39041290+TheCodeGMaster@users.noreply.github.com>
2019-12-14 16:40:47 -0500

    Add files via upload
    Allows users to enter their own input and create a binary Tree. Tried my best working on how to fix the input but decided i'm going to move on with another project. If I ever get time I'll come back and try to fix this one.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <iterator>
 #include "Node.h"
 using namespace std;
 
 int main()
 {
    btree userTree;
-   float x;
+   int a,b,c,x,y;
    char input='Y';'N';
-    int a;
-   cout<<"Enter values you want to see in your tree "<<endl;
-   cin >>x;
 
-    cout<<"Want to enter another number? Enter 'Y' or 'N' "<<endl;
-    cin>>input;
+                                                                ///Allows users to enter their own input instead of it being pre-set values
+
+
+
+cout<<"Enter values you want to see in your tree \n "<<endl;
+cout<<"Must be numbers or program will not run properly\n "<<endl;
+cin>>a>>b>>c>>x;
+
+
+cout<<"Want to enter another number? Enter 'Y' or 'N' "<<endl;
+cin>>input;
+
+
+
+
+
 switch(input){
 
     case 'Y':
@@ -21,29 +35,42 @@ switch(input){
     if(input == 'Y')
     {
     cout<<"Enter values you want to see in your tree "<<endl;
-    cin >>x;
+    cin >>y;
+    userTree.insert(a);
+    userTree.insert(b);
+    userTree.insert(c);
     userTree.insert(x);
+    userTree.insert(y);
     cout<<" "<<endl;
-    userTree.insert(input);
 	userTree.preorder_print();
 	userTree.inorder_print();
 	userTree.postorder_print();
     }
+    break;
     case 'N':
 
         if (input=='N')
         {
+    userTree.insert(a);
+    userTree.insert(b);
+    userTree.insert(c);
     userTree.insert(x);
 	userTree.preorder_print();
 	userTree.inorder_print();
 	userTree.postorder_print();
+
         }
+        cin.get();
+        return 0;
+    if (input!='Y'||input!='N')
+    {
     default:
 
-    cout <<"Error please enter correct input "<<endl;
-
-
-
+    cout<<endl;
+    cout <<"Error please enter either Y or N (Caps please) "<<endl;
+    cout <<"thanks "<<endl;
+    cout <<endl;
+    }
 }
 
 
